eebbfdbb1edacf914175f9f2a721c3f8c7256c01
Ye Peisheng <ye_p@worksap.co.jp>
2018-04-04 18:18:36 +0900

    fix: environment var fix

---
+++ example/src/example/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/appengine"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/helloworld", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Byebye world"})
+	})
+
+	http.Handle("/", router)
+
+	appengine.Main()
+}
